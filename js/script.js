89ff7d1a79de0b8c24fda4a23fa5b15dce866248
Sander Smedbøl <sander.smedbol@stud.noroff.no>
2022-01-31 11:09:27 +0100

    fixed 8

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ for (i = 0; i < paragraphs.length; i++) {
 
 const resultsContainer = document.querySelector(".results");
 resultsContainer.innerHTML = `<p>New paragraph</p>`;
-resultsContainer.style.background = "yellow";
+resultsContainer.style.backgroundColor = "yellow";
 
 //Question 7
 
@@ -60,27 +60,70 @@ catsList(cats);
 
 //Question 8
 
+// function createCats(cats) {
+//   var catContainer = document.querySelector(".cat-container");
+//   for (i = 0; i < cats.length; i++) {
+//     if (typeof cats[i].age === "undefined") {
+//       catContainer.innerHTML +=
+//         "<div><h5>" +
+//         "Name: " +
+//         cats[i].name +
+//         "</h5><p>" +
+//         "Age: Age unknown</p></div>";
+//     } else {
+//       catContainer.innerHTML +=
+//         "<div><h5>" +
+//         "Name: " +
+//         cats[i].name +
+//         "</h5><p>" +
+//         "Age: " +
+//         cats[i].age +
+//         "</p></div>";
+//     }
+//   }
+// }
+
+// createCats(cats);
+
+//question 8 . 2
+
+// Question 8:
+
+// Create a function called createCats. The function will have one parameter called cats.
+
+// Inside the function loop through the value passed in as cats and create HTML for each object in the array.
+
+// Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
+
+// If the age property is missing, it should display “Age unknown” instead.
+
+// Return the HTML from the function.
+
+// Call the function and pass in the cats array as the argument.
+
+// Assign the return value of the function to the innerHTML property of the element on the HTML page with a class of cat-container.
+
 function createCats(cats) {
-  var catContainer = document.querySelector(".cat-container");
+  let myCatItem = "";
   for (i = 0; i < cats.length; i++) {
-    if (typeof cats[i].age === "undefined") {
-      catContainer.innerHTML +=
-        "<div><h5>" +
-        "Name: " +
-        cats[i].name +
-        "</h5><p>" +
-        "Age: Age unknown</p></div>";
-    } else {
-      catContainer.innerHTML +=
-        "<div><h5>" +
-        "Name: " +
-        cats[i].name +
-        "</h5><p>" +
-        "Age: " +
-        cats[i].age +
-        "</p></div>";
-    }
+    myCatItem =
+      myCatItem +
+      `<div>
+    <h5>${cats[i].name}</h5>
+    <p>${cats[i].age ? cats[i].age : "Age unknown"}</p>
+    </div>`;
   }
+  return myCatItem;
 }
 
-createCats(cats);
+const myCats = createCats(cats);
+
+const myCatsContainer = document.querySelector(".cat-container");
+
+myCatsContainer.innerHTML = myCats;
+
+//ekstra
+
+const background = document.querySelector("body");
+
+background.style.background = "grey";
